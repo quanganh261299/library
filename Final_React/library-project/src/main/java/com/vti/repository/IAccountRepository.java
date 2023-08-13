f9790250073b216e782ad34014c718f04bf48a45
coderModeOn <82034865+quanganh261299@users.noreply.github.com>
2023-08-13 12:10:34 +0700

    Delete Final_React/library-project directory

--- Final_React/library-project/src/main/java/com/vti/repository/IAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vti.repository;
-
-import com.vti.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAccountRepository extends JpaRepository<Account, Integer> {
-}
--- Final_React/library-project/src/main/java/com/vti/repository/IBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vti.repository;
-
-import com.vti.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBookRepository extends JpaRepository<Book, Integer> {
-}
